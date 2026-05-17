3e1d34838f8b6b89c66e1d8bcb7ce45fd5df6fca
JuanPabloGHC <jpghc552@hotmail.com>
2026-05-17 14:28:01 -0600

    ci: Updating my experience

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -2,11 +2,20 @@ import type { Experience } from "types/experience";
 
 export const experiences: Experience[] = [
     {
+        title: "Customer Success Engineer",
+        company: "ROOK",
+        dateStart: new Date(2025, 7, 11),
+        dateEnd: new Date(),
+        current: true,
+        description: "Helping companies integrate wearable data into their apps through ROOK's API, SDK, and webhooks. Supporting technical integrations, troubleshooting and guidance.",
+        aptitudes: ["root cause analysis", "problem-solving", "communication", "active listening"]
+    },
+    {
         title: "ALDS & Apps Trainee",
         company: "Schaeffler",
         dateStart: new Date(2024, 4, 13),
-        dateEnd: new Date(),
-        current: true,
+        dateEnd: new Date(2025, 7, 4),
+        current: false,
         description: "Development of web apps using .NET, Entity Framework and SQL Server for backend and Angular for frontend, desktop apps and windows service using .NET Framework.",
         aptitudes: ["teamwork", "problem-solving", "adaptability", "responsability", "communication"]
     },
